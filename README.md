8be839f83413ac4fd92961b9f13782dbb9bdf005
김남준 <njkim@zuminternet.com>
2019-03-18 15:30:07 +0900

    initialize project, README.md upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spring Common Validation Module Example
+
+* 브라우저에서는 얼마든지 요청 데이터를 조작해서 클라이언트 사이드의 validation check를 의미없게 할 수 있다.
+* 따라서, 백엔드에서 필수적으로 validation check를 해야 하는데, @Valid 어노테이션으로 처리하기에 한계가 있는 부분들을 커버하기 위해 공통 Validation 모듈로 처리할 수 있다.
+## Reference
+* https://jojoldu.tistory.com/129
---
+++ src/main/java/io/namjune/commonvalidation/CommonValidationApplication.java
@@ -0,0 +1,13 @@
+package io.namjune.commonvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonValidationApplication.class, args);
+    }
+
+}
