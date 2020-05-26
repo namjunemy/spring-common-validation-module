11a250610865a3bcb97449b1fec6e9fe5096c9fd
Namjun <namjunemy@gmail.com>
2020-05-26 14:00:47 +0900

    constructor 정리

--- src/main/java/io/namjune/commonvalidation/controller/MemberController.java
+++ src/main/java/io/namjune/commonvalidation/controller/MemberController.java
@@ -5,19 +5,18 @@ import io.namjune.commonvalidation.dto.MemberResponseDto;
 import io.namjune.commonvalidation.service.MemberService;
 import java.util.List;
 import javax.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class MemberController {
 
-    private MemberService memberService;
-
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
+    private final MemberService memberService;
 
     @PostMapping("/member")
     public Long saveMember(@RequestBody @Valid MemberRequestDto memberRequestDto) {
